1dcbd2d9b3b65fff938d1a0a77b6e2117fabc2db
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-23 17:04:30 +0530

    ENabled swagger UI

--- src/main/java/org/iitbact/cc/CovidControlApplication.java
+++ src/main/java/org/iitbact/cc/CovidControlApplication.java
@@ -15,8 +15,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CovidControlApplication {
 
 	@Autowired
