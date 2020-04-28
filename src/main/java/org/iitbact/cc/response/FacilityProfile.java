63b8cc19cb33df2288f954c5802ea5021b973f2e
Vinod Talapa <vinodtalapa002@gmail.com>
2020-04-28 11:03:56 +0530

    null pointer in contact details

--- src/main/java/org/iitbact/cc/response/FacilityProfile.java
+++ src/main/java/org/iitbact/cc/response/FacilityProfile.java
@@ -2,6 +2,7 @@ package org.iitbact.cc.response;
 
 import org.iitbact.cc.beans.BaseBean;
 import org.iitbact.cc.beans.ContactDetails;
+import org.iitbact.cc.beans.ContactDetails.ContactDetailsBuilder;
 import org.iitbact.cc.constants.Constants;
 import org.iitbact.cc.entities.Facility;
 
@@ -24,7 +25,24 @@ public class FacilityProfile implements BaseBean<Facility> {
         Object contact=facility.getFacilityContact().getData();
         ObjectMapper mapper=new ObjectMapper();
         JsonNode node=mapper.valueToTree(contact);
-        this.contactDetails= new ContactDetails(node.get(Constants.PRIMARY_CONTACT_NAME).asText("")
-        		,node.get(Constants.PRIMARY_CONTACT_MOBILE).asText(""),node.get(Constants.PRIMARY_CONTACT_EMAIL).asText(""));
+        if(node!=null) {
+        	  ContactDetailsBuilder details=ContactDetails.builder();
+        	  if(node.get(Constants.PRIMARY_CONTACT_NAME)!=null) {
+        		 details= details.name(node.get(Constants.PRIMARY_CONTACT_NAME).asText());
+        		  
+        	  }
+        	  if(node.get(Constants.PRIMARY_CONTACT_EMAIL)!=null) {
+        		  details=  details.name(node.get(Constants.PRIMARY_CONTACT_EMAIL).asText());
+        		  
+        	  }
+        	  
+        	  if(node.get(Constants.PRIMARY_CONTACT_MOBILE)!=null) {
+        		  details=details.name(node.get(Constants.PRIMARY_CONTACT_MOBILE).asText());
+        		  
+        	  }
+        	 this.contactDetails= details.build();
+        
+        }
+       
     }
 }
