ee0ba05e20f4416e2392a56dd9a83cf839306acf
Vinod Talapa <vinodtalapa002@gmail.com>
2020-05-07 11:42:34 +0530

    Releasing Patient Flow (#41)
    * UserId introduced in service for further validations
    
    * UserId introduced in service for further validations
    
    * Adds support for facility linking
    
    * Adding bidirectional link
    
    * Undo log level in dev
    
    * Added Region & search criteria in facility listing
    
    * Contact details added in each facility response
    
    Test facility add/edit apis
    
    * change in swagger url
    
    * change in url for links/get
    
    * null pointer handled
    
    * null pointer in contact details
    
    * Fixing contact details
    
    * Fixing contact details
    
    * Contact details optional
    
    * Adding list areas api
    
    * Facility profile
    
    * Fixing covid facility type
    
    * Region added as filter
    
    * change in swagger host
    
    * Setting default values
    
    * Setting unassigned in prepersist
    
    * Linking extra ward fields
    
    * Adding search criteria for name
    
    * fixing npe
    
    * minor fix with responses (#28)
    
    * Adding has_links field
    
    * Adding DB changes in changes.sql
    
    * To Bug is Human, to Debug is Divine
    
    * Facility listing feature (#33)
    
    * minor fix with responses
    
    * patient flow for admin web with fliters added
    
    * Added severity - asymptomatic in live patient search criteria (#36)
    
    * Added facility search (#37)
    
    * Added severity - asymptomatic in live patient search criteria
    
    * Added facility Search in patients
    
    * Dev branch vinodtalapa (#38)
    
    * Added severity - asymptomatic in live patient search criteria
    
    * Added facility Search in patients
    
    * Area api bug fixed
    
    * Area Api changed (#39)
    
    * Added severity - asymptomatic in live patient search criteria
    
    * Added facility Search in patients
    
    * Area api bug fixed
    
    * Dev branch vinodtalapa (#40)
    
    * Added severity - asymptomatic in live patient search criteria
    
    * Added facility Search in patients
    
    * Area api bug fixed
    
    * Added severity - asymptomatic in live patient search criteria
    
    * Added facility Search in patients
    
    * Area api bug fixed
    
    * Bug fixed
    
    Co-authored-by: siddharth-bulia <siddharth.bulia@meesho.com>
    Co-authored-by: Siddharth Bulia (Meesho) <57344440+siddharth-bulia@users.noreply.github.com>
    Co-authored-by: Piyush Kumar <piyushkumar1523@gmail.com>

--- src/main/java/org/iitbact/cc/controllers/AreaController.java
+++ src/main/java/org/iitbact/cc/controllers/AreaController.java
@@ -1,6 +1,5 @@
 package org.iitbact.cc.controllers;
 
-import io.swagger.annotations.ApiOperation;
 import org.iitbact.cc.beans.ResponseBean;
 import org.iitbact.cc.entities.Area;
 import org.iitbact.cc.helper.ControllerWrapper;
@@ -12,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/areas")
 public class AreaController {
 
     private final AreaService areaService;
     private final ControllerWrapper controllerWrapper;
-
+    
     public AreaController(AreaService areaService, ControllerWrapper controllerWrapper) {
         this.areaService = areaService;
         this.controllerWrapper = controllerWrapper;
@@ -31,6 +32,5 @@ public class AreaController {
         return controllerWrapper.wrap(ListResponse::new, areaRequest,
                 (uid) -> areaService.listAreas(uid));
     }
-
 
 }
