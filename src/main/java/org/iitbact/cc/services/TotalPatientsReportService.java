eda5964f7165dd4cfda7ff62925cd6f5bac41c68
Shailesh Patil <shaileshepatil@gmail.com>
2020-05-14 11:45:45 +0530

    Resolve conflicts

--- src/main/java/org/iitbact/cc/services/TotalPatientsReportService.java
+++ src/main/java/org/iitbact/cc/services/TotalPatientsReportService.java
@@ -10,18 +10,14 @@ import javax.persistence.Query;
 import org.iitbact.cc.entities.AdminUser;
 import org.iitbact.cc.exceptions.CovidControlException;
 import org.iitbact.cc.requests.CommonReportCriteria;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class TotalPatientsReportService {
 
-	
 	private static final Logger LOGGER = LoggerFactory.getLogger(PatientDischargeReportService.class);
-
 
 	@PersistenceContext
 	private EntityManager em;
@@ -51,19 +47,16 @@ public class TotalPatientsReportService {
 
 		entries.add(new String[] { "#", "Facility Name", "Patients Admitted", "Patients Discharged" });
 
-		String qlString = "select admission.name, admission.admission_count, discharged.discharged_count " 
-		        +"from ( " 
+		String qlString = "select admission.name, admission.admission_count, discharged.discharged_count " + "from ( "
 				+ "select  facility.name, count(distinct patients.name) as admission_count "
 				+ "from patients patients  inner join patient_history history on history.patient_id=patients.patient_id  "
 				+ "inner join facilities facility on history.facility_id=facility.facility_id "
-				+ "where  facility.jurisdiction = ?"
-				+ "group by  facility.name " + ") admission " 
-				+ "LEFT join " + "("
+				+ "where  facility.jurisdiction = ?" + "group by  facility.name " + ") admission " + "LEFT join " + "("
 				+ "select  facility.name, count(discharged.creation_time) as discharged_count "
 				+ "from patients patients  inner join patient_discharged discharged on discharged.patient_id=patients.patient_id  "
 				+ "inner join facilities facility on discharged.facility_id=facility.facility_id  "
-				+ "where discharged.reason=\"DISCHARGED\" and facility.jurisdiction = ?" + "group by facility.name " + ") discharged "
-				+ "on admission.name=discharged.name";
+				+ "where discharged.reason=\"DISCHARGED\" and facility.jurisdiction = ?" + "group by facility.name "
+				+ ") discharged " + "on admission.name=discharged.name";
 
 		Query query = em.createNativeQuery(qlString);
 		query.setParameter(1, commonReportCriteria.getJurisdiction());
@@ -72,17 +65,17 @@ public class TotalPatientsReportService {
 
 		Integer dischargedCount;
 		Integer admittedCount;
-		Integer totalDischargedCount =0;
-		Integer totalAdmittedCount =0;
+		Integer totalDischargedCount = 0;
+		Integer totalAdmittedCount = 0;
 		// Add the data rows.
 		for (Object[] p : resultList) {
 			admittedCount = p[1] == null ? 0 : Integer.parseInt(p[1].toString());
 			dischargedCount = p[2] == null ? 0 : Integer.parseInt(p[2].toString());
-			
-			totalAdmittedCount +=admittedCount;
-			totalDischargedCount +=dischargedCount;
 
-			entries.add(new String[] {"", p[0].toString(), admittedCount.toString(), dischargedCount.toString() });
+			totalAdmittedCount += admittedCount;
+			totalDischargedCount += dischargedCount;
+
+			entries.add(new String[] { "", p[0].toString(), admittedCount.toString(), dischargedCount.toString() });
 		}
 		entries.add(new String[] { "Total", "-", totalAdmittedCount.toString(), totalDischargedCount.toString() });
 
